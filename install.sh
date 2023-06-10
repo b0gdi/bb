0c9e304aa3240380e30f5bb3e251d15fa24ef5bb
b0gdi <97564989+b0gdi@users.noreply.github.com>
2023-06-10 16:11:01 +0300

    install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+d22.04 
+
+piton
+go
+ifconfig
+
+https://wordlists.assetnote.io/
+https://github.com/danielmiessler/SecLists
+
+waymore + linkfinder
+whatweb
+aquanote (screen shots)
+nmap
+tcpdump
+need some SSL tools
+
+feroxbuster
+ffuf
+subfinder
+assetfinder
+wp-scan
+JSParser
+
+
+nuclei
+sqlmap
+hydra
